bf4f9b3c11a5e8f63794a320b66541e57e8ad3ce
chavp <my.parinya@gmail.com>
2015-01-28 09:10:53 +0700

    first project
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ CIWithMe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIWithMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CIWithMe ver.0.0.0");
+            Console.ReadLine();
+        }
+    }
+}
